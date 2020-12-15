3c383e4040af8b32f2bc3183cafee61b56ae43fa
Captain Chicky <maxfang2015@yahoo.com>
2020-12-14 20:32:11 -0700

    Update Delete Adobe Genuine Service Popup.cmd

--- Delete Adobe Genuine Service Popup.cmd
+++ Delete Adobe Genuine Service Popup.cmd
@@ -1,3 +1,30 @@
+@echo off
+
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+echo Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+echo args = "ELEV " >> "%vbsGetPrivileges%"
+echo For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+echo args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+echo Next >> "%vbsGetPrivileges%"
+echo UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /b
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
 if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
 cls
 goto Start
@@ -32,6 +59,9 @@ Rmdir /s /q "C:\Program Files (x86)\Common Files\Adobe\Adobe Desktop Common\Adob
 echo ======================================================================
 echo Now that the above is removed, would you like to also remove the logs? 
 echo (Please note that logs can help with diagnosis of problems, amongst others.)
+goto Choice
+
+:Choice
 set /p "choice=Y or N?..."
 if /i "%choice%" == "Y" (
     	goto DelLog
@@ -41,8 +71,7 @@ if /i "%choice%" == "Y" (
 	echo Your input is not one of the choices. 
 	echo Please try again...
 	pause
-	cls
-	goto Pre
+	goto Choice
 )
 
 :DelLog
@@ -71,6 +100,6 @@ echo This will cause no harm to your computer, so don't worry :)
 echo ======================================================================
 echo I would like to give special thanks to the tool "Everything", v1.4.1.1000, by voidtools.
 echo This tool makes an index of the entire file system, and makes it extremely fast and easy to search for files.
-echo If you are interested, please visit "https://www.voidtools.com/"! :D
+echo If you are interested, please visit "https://www.voidtools.com/". :)
 pause
 exit
